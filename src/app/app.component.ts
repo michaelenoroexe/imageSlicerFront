308e272da1f8aab897e0f6ddd1d4523e7b96836b
Michaelenoro <serezha.alexandrovitch2018@yandex.ru>
2022-05-18 22:39:54 +0300

    Created submit button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,13 @@ import { InstructionSenderService } from './requests/instructionsPost/post.servi
   providers: [InstructionSenderService],
 })
 export class AppComponent {
-  img:any;
+  img:File | string = "";
   displayImagePath:path = new path();
   recievedImage:path = new path();
   dropAreaHighlight:boolean = false;
   formats = [{viewValue: "A0", value: "A0", maxColNum: 2},{viewValue: "A1", value: "A1", maxColNum: 3},{viewValue: "A2", value: "A2", maxColNum: 5},{viewValue: "A3", value: "A3", maxColNum: 8},{viewValue: "A4", value: "A4", maxColNum: 10},{viewValue: "A5", value: "A5", maxColNum: 15},{viewValue: "A6", value: "A6", maxColNum: 20}]
   selectedFormat:string | undefined = "A4";
-  
+  // Format that user select
   public get SelectedFormat() {
     return this.selectedFormat;
   }
@@ -23,17 +23,16 @@ export class AppComponent {
     this.SetMaxColumns(this.formats.find(ar=>ar.value==val)?.maxColNum);
     this.selectedFormat = this.formats.find(ar=>ar.value==val)?.value;
   }
-
   selectedColNumber:number = 1;
   maxColomnNumber = new Array(this.formats[5].maxColNum);  
   defaultColomnNumber:number = 1;
+
   // Display on page users selected image 
   OnFileSelected(event:any) {
     let ab = this.displayImagePath;
-    let ia = <File>event.dataTransfer.files[0];
-    this.UploadFile(ia);
+    this.img = <File>event.dataTransfer.files[0];
     let reader = new FileReader();
-    reader.readAsDataURL(ia);
+    reader.readAsDataURL(this.img);
     reader.onload = function() {
       ab.str = reader.result;
     };
@@ -61,12 +60,13 @@ export class AppComponent {
     this.OnFileSelected(e);
   }
 
-  UploadFile(file:File) {
+  UploadFile(event:Event) {
+    event.preventDefault();
     let formData = new FormData();
     let arbRef = this.recievedImage;
-    formData.append('file', file);
-    formData.append('type', 'A4');
-    formData.append('colNum', '3');
+    formData.append('file', this.img);
+    formData.append('type', ''+this.selectedFormat);
+    formData.append('colNum',''+this.selectedColNumber);
     formData.append('orientation', 'landscape');
     let ar = this.instrSendeService.PostInst(formData);
     console.log(ar);
